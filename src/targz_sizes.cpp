e6cb773987ee0c8b5e55d582d19caaf3d08a83d8
Tomasz Melcer <liori@exroot.org>
2012-11-07 22:28:20 +0100

    Empty project as generated by Eclipse.

---
+++ src/targz_sizes.cpp
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : targz_sizes.cpp
+ Author      : Tomasz Melcer
+ Version     :
+ Copyright   : GPL v3
+ Description : Hello World in C++,
+ ============================================================================
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	cout << "Hello World" << endl; /* prints Hello World */
+	return 0;
+}
