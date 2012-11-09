b24854620507ff161d4b62a56a997430267b2f76
Tomasz Melcer <liori@exroot.org>
2012-11-09 22:44:39 +0100

    Switch to C.
    In such a simple project the complexity of C++ is unnecessary. Also,
    handling IO in C++ is a little mess, with things like being unable to
    reopen std::cin in binary mode in a standard way.

--- src/targz_sizes.c
+++ src/targz_sizes.c
@@ -1,6 +1,6 @@
 /*
  ============================================================================
- Name        : targz_sizes.cpp
+ Name        : targz_sizes.c
  Author      : Tomasz Melcer
  Version     :
  Copyright   : GPL v3
@@ -8,11 +8,8 @@
  ============================================================================
  */
 
-#include <iostream>
-#include <cstdio>
+#include <stdio.h>
 #include <zlib.h>
-
-using namespace std;
 
 #define COMPRESSED_BUFFER 102400
 
@@ -34,25 +31,21 @@ long long decode_octal(char* size) {
 int main(int argc, char** argv) {
     // argument parsing
     if (argc != 2) {
-        cout << "Usage:" << endl
-             << "   " << argv[0] << " file.tar.gz" << endl;
-
+    	// TODO: put this to stderr
+    	printf("Usage:\n   %s file.tar.gz\n", argv[0]);
         return 1;
     }
 
     char* filename = argv[1];
 
     // opening files and setting up counters
-    std::cout << "open " << filename << endl;
     FILE* file = fopen(filename, "rb");  // TODO: error checking
 
     unsigned char compressed[COMPRESSED_BUFFER];
-    tarheader decompressed;
+    struct tarheader decompressed;
     gz_header gzip_header = {0};
     z_stream gzip_stream = {0};
     long long int bytes_read = 0;
-
-#define ERROR " errmsg: " << (gzip_stream.msg ? gzip_stream.msg : "none") << endl
 
     gzip_stream.next_in = compressed;
     gzip_stream.avail_in = 0;
@@ -60,21 +53,17 @@ int main(int argc, char** argv) {
     gzip_stream.zfree = Z_NULL;
     gzip_stream.next_out = (unsigned char*) &decompressed;
     gzip_stream.avail_out = sizeof(decompressed);
-    std::cout << "inflateInit: " << inflateInit2(&gzip_stream, 15+32) << ERROR;  // TODO: error checking
-    std::cout << "inflateGetHeader: " << inflateGetHeader(&gzip_stream, &gzip_header) << ERROR;
-    //std::cout << "tar block size: " << sizeof(decompressed) << endl;
+    inflateInit2(&gzip_stream, 15+32); // TODO: error checking
+    inflateGetHeader(&gzip_stream, &gzip_header);
 
     // state machine
     int skip_blocks = 0;
-    while(true) {
-        //std::cout << "new cycle\n";
+    while(1) {
         // if stream's avail_in is empty, we need more data.
         if (gzip_stream.avail_in == 0) {
-            //std::cout << "Empty input." << endl;
             // TODO: error checking
             // TODO: end of file condition
             size_t bytes = fread(&compressed, 1, COMPRESSED_BUFFER, file);
-            //std::cout << "Read " << bytes << " bytes.\n";
             bytes_read += bytes;
             gzip_stream.next_in = compressed;
             gzip_stream.avail_in = bytes;
@@ -83,30 +72,24 @@ int main(int argc, char** argv) {
 
         // if output buffer is not full, try to decompress more data
         if (gzip_stream.avail_out > 0) {
-            //std::cout << "free space for decompression: " << gzip_stream.avail_out << endl;
             // TODO: error checking
-            // TODO: end of stream condition
-            int cond;
-            std::cout << "inflate: " << (cond = inflate(&gzip_stream, Z_NO_FLUSH)) << ERROR;
+            int cond = inflate(&gzip_stream, Z_NO_FLUSH);
             if (cond == 1)
                 return 0;
         }
 
         // if output buffer is full, perform an action.
         if (gzip_stream.avail_out == 0) {
-            //std::cout << "Full buffer. ";
             if (skip_blocks > 0) {
                 decompressed.filename[20] = 0;
-                //std::cout << "skipped: [[[" << (char*) &decompressed << "]]]" << endl;
                 // we're inside a file. just ignore the block
                 skip_blocks--;
             } else {
                 // we've got a header. print details.
                 // TODO: handle long file names.
-                std::cout << "File: " << decompressed.filename
-                          << ", decompressed size: " << decompressed.size
-                          << ", position: " << gzip_stream.total_in
-                          << endl;
+            	printf("File: %s, decompressed size: %s, position: %ld\n",
+            		decompressed.filename, decompressed.size,
+            		gzip_stream.total_in);
 
                 long long size = decode_octal(decompressed.size);
                 skip_blocks = ((size + 511) / 512);
