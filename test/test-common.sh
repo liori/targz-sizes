146678c6d32618c5ac9b05f306ae94e72df47571
bitcog <dev-dct@bitcog.com>
2016-06-04 14:32:23 +1000

    Added support and unit tests for long file names.

--- test/test-common.sh
+++ test/test-common.sh
@@ -7,8 +7,8 @@ targz_sizes=${top_srcdir}/src/targz_sizes
 
 prepTestDir () {
 
-	rm -rf ${test_dir}
-	mkdir ${test_dir}
+    rm -rf ${test_dir}
+    mkdir ${test_dir}
 
 }
 
@@ -18,13 +18,13 @@ runTest () {
     echo "runTest(), run command: ${LOG_COMPILER} ${LOG_FLAGS} ${TEST_COMMAND} < ${test_dir}/input.tgz > ${test_dir}/rawout 2> ${test_dir}/rawerr"
     ${LOG_COMPILER} ${LOG_FLAGS} ${TEST_COMMAND} < ${test_dir}/input.tgz 1> ${test_dir}/rawout 2> ${test_dir}/rawerr 
     command_result=$?
-	cat ${test_dir}/rawerr ${test_dir}/rawout
+    cat ${test_dir}/rawerr ${test_dir}/rawout
     echo "runTest(), command_result=$command_result"
     if [ "$command_result" != "0" ] ; then
         echo "runTest(), exiting"
         exit $command_result
     fi
-	cp ${test_dir}/rawout ${test_dir}/output
+    cp ${test_dir}/rawout ${test_dir}/output
 }
 
 checkFilenames () {
@@ -32,7 +32,7 @@ checkFilenames () {
     echo "checkFilenames(), entered, test_dir=$test_dir"
 
     # create expected result
-    tar tzf ${test_dir}/input.tgz > ${test_dir}/expout
+    tar tzf ${test_dir}/input.tgz | cut -c 1-1023 > ${test_dir}/expout
 
     echo "checkFilenames(), run diff: diff ${test_dir}/output ${test_dir}/expout"
     diff ${test_dir}/output ${test_dir}/expout
@@ -52,7 +52,7 @@ checkInErrOut () {
 
     echo "checkInErrOut(), run egrep: egrep \"$1\" ${test_dir}/rawerr"
     egrep "$1" ${test_dir}/rawerr
-	egrep_result=$?
+    egrep_result=$?
 
     echo "checkInErrOut(), egrep_result=$egrep_result"
 
@@ -62,3 +62,25 @@ checkInErrOut () {
     fi
 }
 
+makeName () {
+
+    count=$1
+
+    for i in `seq 1 ${count}`; do
+
+        if [ $i = 1 ] ; then
+            echo -n '['
+        elif [ $i = ${count} ] ; then
+            echo -n ']'
+        elif [ $((i%100)) = 0 ] ; then
+            echo -n '/'
+        elif [ $((i%10)) = 0 ] ; then
+            echo -n $((i/10%10))
+        elif [ $((i%5)) = 0 ] ; then
+            echo -n ','
+        else
+            echo -n '.'
+        fi
+
+    done
+}
